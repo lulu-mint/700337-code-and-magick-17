92f2f3ddeabdea8b3c747c626300f09dc92ba7bf
Lulu <kya97@mail.ru>
2019-06-07 23:55:35 +0200

    пофикшено по eslinter

--- js/stat.js
+++ js/stat.js
@@ -1,15 +1,17 @@
+'use strict';
+
 var GISTO_X = 100;
 var GISTO_Y = 240;
 var COL_WIDTH = 40;
 var GAP_COL = 50;
 var GAP_ROW = 20;
 
-var drawPopup = function(ctx, color, x, y){
+var drawPopup = function (ctx, color, x, y) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, 420, 270);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -21,7 +23,7 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   drawPopup(ctx, 'rgba(0, 0, 0, 0.7)', 110, 20);
   drawPopup(ctx, 'white', 100, 10);
 
@@ -37,15 +39,14 @@ window.renderStatistics = function(ctx, names, times) {
     ctx.textBaseline = 'bottom';
     ctx.fillStyle = 'black';
     ctx.fillText(names[i], (GISTO_X + GAP_COL + (COL_WIDTH + GAP_COL) * i), GISTO_Y + GAP_ROW);
-    ctx.fillText(parseInt(times[i]), (GISTO_X + GAP_COL + (COL_WIDTH + GAP_COL) * i), GISTO_Y - ((150 * times[i]) / maxTime)) - GAP_ROW*2;
+    ctx.fillText(parseInt(times[i], 10), (GISTO_X + GAP_COL + (COL_WIDTH + GAP_COL) * i), (GISTO_Y - ((150 * times[i]) / maxTime)) - GAP_ROW * 2);
 
-    if (names[i] == 'Вы'){
+    if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    }else{
-      var RAND_OPACITY = parseInt(Math.random()*10) / 10;
-      console.log(RAND_OPACITY);
-      ctx.fillStyle = 'rgba(255, 0, 0, RAND_OPACITY)';
+    } else {
+      var RAND_OPACITY = parseInt(Math.random() * 10, 10) / 10;
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + RAND_OPACITY + ')';
     }
-    ctx.fillRect( (GISTO_X + GAP_COL + (COL_WIDTH + GAP_COL) * i), GISTO_Y - ((150 * times[i]) / maxTime), 40, (150 * times[i]) / maxTime);
+    ctx.fillRect((GISTO_X + GAP_COL + (COL_WIDTH + GAP_COL) * i), GISTO_Y - ((150 * times[i]) / maxTime), 40, (150 * times[i]) / maxTime);
   }
 };
